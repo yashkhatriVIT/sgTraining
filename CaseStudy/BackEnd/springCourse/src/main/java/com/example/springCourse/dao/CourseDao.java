826ab19eeb78b37f75a1f4b4b3b3355e47299ec8
yashkhatriVIT <55588209+yashkhatriVIT@users.noreply.github.com>
2022-06-27 17:02:45 +0530

    Delete CaseStudy directory

--- CaseStudy/BackEnd/springCourse/src/main/java/com/example/springCourse/dao/CourseDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.springCourse.dao;
-
-import com.example.springCourse.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-
-}
